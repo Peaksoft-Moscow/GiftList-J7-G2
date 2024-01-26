3c1256dea50fe83f17f0505093c5b168c31c9c77
timurlankasymbaev <moscow.peaksoft@gmail.com>
2024-01-26 17:48:47 +0300

    Initial commit

---
+++ src/test/java/peaksoft/giftlistj7g2/GiftListJ7G2ApplicationTests.java
@@ -0,0 +1,13 @@
+package peaksoft.giftlistj7g2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftListJ7G2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
