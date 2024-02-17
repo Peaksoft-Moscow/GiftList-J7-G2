bdb607f8e21b35b1e646d1cd2e0d4e75cdcce6fb
begimai <rionayuko4@gmail.com>
2024-02-17 19:26:26 +0300

    SingIn

--- src/main/java/peaksoft/giftlistj7g2/model/enums/Role.java
+++ src/main/java/peaksoft/giftlistj7g2/model/enums/Role.java
@@ -1,23 +1,13 @@
 package peaksoft.giftlistj7g2.model.enums;
 
-import jakarta.persistence.*;
-import lombok.*;
-import lombok.experimental.FieldDefaults;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-
-import java.util.HashSet;
-import java.util.Set;
-
 
 public enum Role implements GrantedAuthority {
-    ADMIN(),
-    USER();
+    ADMIN,
+    USER;
 
     @Override
     public String getAuthority() {
         return name();
     }
-
 }
-
